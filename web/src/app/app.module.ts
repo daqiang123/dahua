d6e0bd92fd7fc1a2778dc9476ad1cc83e1807f5e
daqiang123 <361604199@qq.com>
2018-12-01 16:23:03 +0800

    修改优化代码

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,6 +5,7 @@ import * as $ from 'jquery';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +31,12 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+	{
+		provide:LocationStrategy, 
+		useClass: HashLocationStrategy
+	}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
